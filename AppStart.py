98287609b107bfa07f8607383c31c9ed51510730
declife <liu806837556@gmail.com>
2020-11-03 13:08:22 +0800

    delete proxy

--- AppStart.py
+++ AppStart.py
@@ -4,10 +4,10 @@ import logging
 from telegram.ext import Updater
 from Callback.Register import register_handler
 
-REQUEST_KWARGS = {
-    # "USERNAME:PASSWORD@" is optional, if you need authentication:
-    'proxy_url': 'http://127.0.0.1:7890/',
-}
+# REQUEST_KWARGS = {
+#     # "USERNAME:PASSWORD@" is optional, if you need authentication:
+#     'proxy_url': 'http://127.0.0.1:7890/',
+# }
 
 
 def main():
@@ -15,8 +15,9 @@ def main():
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO)
     updater = Updater(token='1114865886:AAGvoN6GZoykqzVQNrG7NjiUhNrs1HC9Igg',
-                      use_context=True,
-                      request_kwargs=REQUEST_KWARGS)
+                      use_context=True)
+                      # use_context=True,
+                      # request_kwargs=REQUEST_KWARGS)
     register_handler(updater)
     updater.start_polling(timeout=5)
 
